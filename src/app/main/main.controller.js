f9f9f080272a2d2c4068061699e825cb906ce818
Nat Weerawan <nat.wrw@gmail.com>
2015-06-26 12:44:40 +0700

    Add new information / host.

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -6,12 +6,12 @@
     .controller('MainController', MainController);
 
   /** @ngInject */
-  function MainController($timeout, webDevTec, toastr) {
+  function MainController($scope, $timeout, webDevTec, toastr) {
     var vm = this;
 
     vm.awesomeThings = [];
     vm.classAnimation = '';
-    vm.creationDate = 1435296432816;
+    vm.creationDate = 1435123596915;
     vm.showToastr = showToastr;
 
     activate();
@@ -35,5 +35,95 @@
         awesomeThing.rank = Math.random();
       });
     }
+
+    $scope.heartbeat = "WAITING...";
+
+    var host = 'test.mosquitto.org'; // hostname or IP address
+    var port = 8080;
+    var useTLS = false;
+    var username = null;
+    var password = null;
+    var cleansession = true;
+    // username = "jjolie";
+    // password = "aa";
+
+    var topic_list = ["esp8266/18:fe:34:fe:c0:ff/#"];
+
+    var mqtt;
+    var reconnectTimeout = 2000;
+    function MQTTconnect() {
+        mqtt = new Paho.MQTT.Client(
+                        host,
+                        port,
+                        "web_" + parseInt(Math.random() * 100,
+                        10));
+        var options = {
+            timeout: 3,
+            useSSL: useTLS,
+            cleanSession: cleansession,
+            onSuccess: onConnect,
+            onFailure: function (message) {
+                console.log("failed");
+                setTimeout(MQTTconnect, reconnectTimeout);
+            }
+        };
+        mqtt.onConnectionLost = onConnectionLost;
+        mqtt.onMessageArrived = onMessageArrived;
+        if (username != null) {
+            options.userName = username;
+            options.password = password;
+        }
+        console.log("Host="+ host + ", port=" + port + " TLS = " + useTLS + " username=" + username + " password=" + password);
+        mqtt.connect(options);
+    }
+    function onConnect() {
+        console.log('Connected to ' + host + ':' + port);
+        angular.forEach(topic_list, function(topic, idx) {
+          console.log("subscribing..", topic);
+          mqtt.subscribe(topic, {qos: 0});
+        });
+    }
+    function onConnectionLost(response) {
+        setTimeout(MQTTconnect, reconnectTimeout);
+        console.log("connection lost: " + response.errorMessage + ". Reconnecting");
+    };
+
+
+    function onMessageArrived(message) {
+        var topic = message.destinationName;
+        var payload = message.payloadString;
+        var json = JSON.parse(payload);
+        
+        if (topic.indexOf("/command") !== false) {
+          if (payload == "0") {
+            $scope.enabled = false;
+          }
+          else if (payload == "1") {
+            $scope.enabled = true;
+          }
+          $scope.$apply();
+        }
+
+        if (json.d && json.d.myName == "TONG")  {
+          $scope.heartbeat = json.d;
+          $scope.$apply();
+          console.log(json.d && json.d.myName);
+        }
+
+    };
+
+    MQTTconnect();
+    $scope.enable = function(state) {
+      var qos = 0;
+      var retain = true;
+      if (state) {
+        mqtt.send("esp8266/18:fe:34:fe:c0:ff/command", "1", qos, retain)
+      }
+      else {
+        mqtt.send("esp8266/18:fe:34:fe:c0:ff/command", "0", qos, retain);
+      }
+    }
+
+
   }
 })();
